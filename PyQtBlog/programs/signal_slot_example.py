7ee472028cd922183c218459551256b48421deb7
Paul <paul.sajo.official@gmailcom>
2021-10-20 12:17:17 +0530

    added signal slot mechanism

--- PyQtBlog/programs/signal_slot_example.py
+++ PyQtBlog/programs/signal_slot_example.py
@@ -2,6 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 
+def change_button_text():
+    button.setText("Button has been clicked")
+
+
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
@@ -12,6 +16,7 @@ if __name__ == '__main__':
 
     label = QLabel("Sample label")
     button = QPushButton("Button") # create an instance of QPushbutton
+    button.clicked.connect(change_button_text)
     
     v_layout.addWidget(label)
     v_layout.addWidget(button)
