d11f07bcd4a59951cfd48c1da709c93c9ce77607
Paul <paul.sajo.official@gmailcom>
2021-10-21 22:39:02 +0530

    modified palette example

--- PyQtBlog/programs/palette_example.py
+++ PyQtBlog/programs/palette_example.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     label = QLabel("hello")
+    label.setFont(QFont("Times new roman", 20))  # change font size, setFont expects an instance of QFont(font_family, Font-size)
+    
     widget_palette = QPalette()
     widget_palette.setColor(QPalette.Window, QColor("red"))
     widget_palette.setColor(QPalette.WindowText, QColor("#00ff00"))
-    label.setFont(QFont("Times new roman", 20))
 
     label.setPalette(widget_palette)
 
